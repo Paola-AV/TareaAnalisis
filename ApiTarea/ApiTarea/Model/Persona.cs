1b7463ac135ac8745bbbb5e0319628edf1e4a299
Paola-AV <vargasp437@gmail.com>
2024-06-04 22:29:28 -0600

    files

---
+++ ApiTarea/ApiTarea/Model/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiTarea.Model
+{
+    public class Persona
+    {
+        [Key]
+
+        public int PersonaID { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido1 { get; set; }
+
+        public string Apellido2 { get; set; }
+
+        public string telefono { get; set; }
+    }
+}
